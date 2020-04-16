9c8f8e2e7a70fb6d892debaf129f792aa07a7146
Robert Masen <robert.masen@smartthings.com>
2020-04-16 16:44:54 -0500

    re-impl logging

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 
-use warp::{filters::fs::dir};
-
+use warp::{filters::fs::dir, Filter, log};
 
 #[tokio::main]
 async fn main() {
+    pretty_env_logger::init();
     let mut args = std::env::args();
     let _ = args.next();
     let root = if let Some(root) = args.next() {
@@ -13,13 +13,15 @@ async fn main() {
     };
     
 
-    let server = warp::serve(dir(root.clone()));
+    let server = warp::serve(dir(root.clone()).with(log("serve")));
     if let Ok((addr, f)) = server.try_bind_ephemeral(([127, 0, 0, 1], 0)) {
         println!("listening on http://127.0.0.1:{}", addr.port());
         f.await;
     } else {
-        let server = warp::serve(dir(root));
         println!("listening on http://127.0.0.1:3456");
-        server.run(([127,0,0,1], 3456)).await;
+        warp::serve(dir(root).with(log("serve")))
+            .run(([127,0,0,1], 3456)).await;
     }
 }
+
+
