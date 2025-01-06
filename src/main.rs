e1edee326006ad0da3f6ff5a4609b5e3958ed969
Robert Masen <r.f.masen@gmail.com>
2025-01-05 20:09:09 -0600

    publish

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use std::{
-    convert::Infallible,
-    path::{Path, PathBuf},
-};
+use std::path::{Path, PathBuf};
 
+use axum::{response::Html, Router};
 use tokio::io::AsyncWriteExt;
-use warp::{filters::fs::dir, log, reply::Html, Filter};
 
 const INDEX_PREFIX: &str = "<!DOCTYPE html><html><head></head><body><main><ul>";
 const INDEX_SUFFIX: &str = "</ul></body></html>";
@@ -12,14 +9,9 @@ const INDEX_SUFFIX: &str = "</ul></body></html>";
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
-    let mut args = std::env::args();
-    let _ = args.next();
-    let root = if let Some(root) = args.next() {
-        PathBuf::from(root)
-    } else {
-        PathBuf::from("./")
-    };
-    let index_path = if !root.join("index.html").exists() {
+    let Args { root, prefix, port } = parse_args();
+
+    let index_path: PathBuf = if !root.join("index.html").exists() {
         let temp = tempfile::Builder::default().tempfile().unwrap();
         let (_l, r) = temp.into_parts();
         let path = r.to_path_buf();
@@ -31,27 +23,77 @@ async fn main() {
     } else {
         root.join("index.html")
     };
-    let index = warp::get()
-        .and(warp::path::end().or(warp::path("index.html")))
-        .and_then(move |_| read_path(index_path.clone()));
+    let index_cb = {
+        let index_path = index_path.clone();
+        let prefix = prefix.clone();
+        let base_path = root.clone();
+        move |uri: axum::http::Uri| async move {
+            log::trace!("getting {uri}");
+            let path = uri.path().trim_start_matches(&prefix);
+            let path = if path == "" || path == "/" || path == "/index.html" || path == "index.htm"
+            {
+                index_path.clone()
+            } else {
+                base_path.join(path)
+            };
+            read_path(path).await
+        }
+    };
 
-    let server = warp::serve(dir(root.clone()).or(index).with(log("serve")));
-    if let Ok((addr, f)) = server.try_bind_ephemeral(([127, 0, 0, 1], 0)) {
-        println!("listening on http://127.0.0.1:{}", addr.port());
-        f.await;
-    } else {
-        println!("listening on http://127.0.0.1:3456");
-        warp::serve(dir(root).with(log("serve")))
-            .run(([127, 0, 0, 1], 3456))
-            .await;
-    }
+    let app = Router::new().fallback(index_cb);
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
+        .await
+        .unwrap();
+    let addr = listener.local_addr().unwrap();
+    let msg = format!("Listening on http://{}:{}", addr.ip(), addr.port());
+    let f = tokio::task::spawn(async move { axum::serve(listener, app).await.unwrap() });
+    log::info!("{msg}");
+    f.await.unwrap();
 }
 
-async fn read_path(path: PathBuf) -> Result<Html<String>, Infallible> {
-    let text = tokio::fs::read_to_string(path.clone())
-        .await
-        .unwrap_or_else(|e| format!("{INDEX_PREFIX}<li><pre><code>{e}</code></pre></li>{INDEX_SUFFIX}"));
-    Ok(warp::reply::html(text))
+struct Args {
+    root: PathBuf,
+    prefix: String,
+    port: u16,
+}
+
+fn parse_args() -> Args {
+    let mut args = std::env::args().skip(1);
+    let root = if let Some(root) = args.next() {
+        PathBuf::from(root)
+    } else {
+        PathBuf::from("./")
+    };
+    let mut ret = Args {
+        root,
+        prefix: "/".to_string(),
+        port: 0,
+    };
+    while let Some(arg) = args.next() {
+        match arg.as_str() {
+            "--prefix" => ret.prefix = args.next().expect("--prefix requires value"),
+            "--port" => {
+                ret.port = args
+                    .next()
+                    .expect("--port requires value")
+                    .parse()
+                    .expect("invalid port")
+            }
+            a => panic!("Unknown argument {}", a),
+        }
+    }
+    ret
+}
+
+async fn read_path(path: PathBuf) -> Html<String> {
+    log::debug!("reading {}", path.display());
+    Html(
+        tokio::fs::read_to_string(path.clone())
+            .await
+            .unwrap_or_else(|e| {
+                format!("{INDEX_PREFIX}<li><pre><code>{e}</code></pre></li>{INDEX_SUFFIX}")
+            }),
+    )
 }
 
 async fn spawn_index_generator(root: PathBuf, index_path: impl AsRef<Path>) {
@@ -63,10 +105,7 @@ async fn spawn_index_generator(root: PathBuf, index_path: impl AsRef<Path>) {
     }
 }
 
-async fn do_index_gen(
-    root: impl AsRef<Path>,
-    index: impl AsRef<Path>,
-) -> Result<(), Error> {
+async fn do_index_gen(root: impl AsRef<Path>, index: impl AsRef<Path>) -> Result<(), Error> {
     if !root.as_ref().exists() {
         ::log::warn!("Root no longer exists at {}", root.as_ref().display());
         return Ok(());
@@ -76,14 +115,16 @@ async fn do_index_gen(
     Ok(())
 }
 
-async fn generate_file_list(
-    path: impl AsRef<Path>,
-) -> Result<Vec<String>, Error> {
-    let mut rd = tokio::fs::read_dir(path.as_ref()).await.map_err(|e| {
-        Error::ReadDir(format!("Error reading {}: {e}", path.as_ref().display()))
-    })?;
+async fn generate_file_list(path: impl AsRef<Path>) -> Result<Vec<String>, Error> {
+    let mut rd = tokio::fs::read_dir(path.as_ref())
+        .await
+        .map_err(|e| Error::ReadDir(format!("Error reading {}: {e}", path.as_ref().display())))?;
     let mut ret = Vec::new();
-    while let Some(entry) = rd.next_entry().await.map_err(|e| Error::Entry(format!("Error looking up next entry: {e}")))? {
+    while let Some(entry) = rd
+        .next_entry()
+        .await
+        .map_err(|e| Error::Entry(format!("Error looking up next entry: {e}")))?
+    {
         let Ok(ft) = entry.file_type().await else {
             continue;
         };
@@ -112,9 +153,7 @@ async fn write_index_html(
             eprintln!("Error opening {}", path.as_ref().display());
             e
         })?;
-    f.write_all(INDEX_PREFIX.as_bytes())
-        .await
-        .ok();
+    f.write_all(INDEX_PREFIX.as_bytes()).await.ok();
     for href in files {
         f.write_all(format!(r#"<li><a href="/{href}">{href}</a></li>"#).as_bytes())
             .await?;
@@ -131,5 +170,4 @@ enum Error {
     ReadDir(String),
     #[error("{0}")]
     Entry(String),
-
 }
